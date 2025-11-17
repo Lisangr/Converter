03e32c9491a7350e71af1e5697e1042928eff1c0
Lisangr <165577888+Lisangr@users.noreply.github.com>
2025-11-17 16:44:43 +0300

    fix2

--- UI/VideoThumbnailControl.cs
+++ UI/VideoThumbnailControl.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Converter.Application.Abstractions;
+using Converter.Models;
 using Xabe.FFmpeg;
 
 namespace Converter.UI
